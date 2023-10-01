7e0be80b96593e18e597ad7e88cff7c3bf1011ff
Tomas Zikmund <tommekster@gmail.com>
2023-10-01 08:38:15 +0200

    icon sizes

--- image2ico/__main__.py
+++ image2ico/__main__.py
@@ -4,7 +4,7 @@ from . import converter
 
 def main():
     args = cli.parse_arguments()
-    converter.convert(args.input, args.output)
+    converter.convert(args.input, args.output, [(x, x) for x in args.sizes])
 
 
 if __name__ == "__main__":
--- image2ico/cli/cli_arguments.py
+++ image2ico/cli/cli_arguments.py
@@ -1,11 +1,17 @@
 from pathlib import Path
+from typing import Literal
 from tap import Tap
 
 
 class CliArguments(Tap):
     input: Path
     output: Path = Path("output.ico")
+    sizes: list[Literal[16, 24, 32, 48, 64, 128, 255]] = [
+        16, 24, 32, 48, 64, 128, 255
+    ]
 
     def configure(self) -> None:
         self.add_argument("input")
         self.add_argument("-o", "--output")
+        self.add_argument(
+            "-s", "--sizes", help="list of icon sizes (ex: --sizes 16 32)")
