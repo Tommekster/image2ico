48b9c7c5cb44ed62de21059e60d1b32ba70b0cd6
Tomas Zikmund <tommekster@gmail.com>
2023-10-01 09:59:30 +0200

    cx_Freeze

---
+++ setup.py
@@ -0,0 +1,41 @@
+import tomli
+from cx_Freeze import setup, Executable
+
+icon = "image2ico.ico"
+
+with open("pyproject.toml", "rb") as f:
+    project = tomli.load(f)
+
+bdist_msi_options = {
+    "data": {
+        "ProgId": [
+            ("Prog.Id", None, None,
+             "Image2ico: Image to icon converting tool", "IconId", None),
+        ],
+        "Icon": [
+            ("IconId", icon),
+        ],
+    },
+    "install_icon": icon,
+    "upgrade_code": "{e23bcc11-1311-4630-adb2-2bb63c0ba45d}",
+    "add_to_path": True
+}
+
+
+setup(
+    name=project["tool"]["poetry"]["name"],
+    version=project["tool"]["poetry"]["version"],
+    description=project["tool"]["poetry"]["description"],
+    author=project["tool"]["poetry"]["authors"][0],
+    options={
+        "build_exe": project["tool"]["distutils"]["build_exe"],
+        "bdist_msi": bdist_msi_options,
+    },
+    executables=[
+        Executable(
+            'cli.py',
+            icon=icon,
+            target_name='image2ico',
+        )
+    ]
+)
