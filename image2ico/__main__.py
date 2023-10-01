306be2e1a0bbbe7f2c574763ca457f9d4bf8d644
Tomas Zikmund <tommekster@gmail.com>
2023-10-01 09:02:49 +0200

    default output: input_image.ico

--- image2ico/__main__.py
+++ image2ico/__main__.py
@@ -4,7 +4,11 @@ from . import converter
 
 def main():
     args = cli.parse_arguments()
-    converter.convert(args.input, args.output, [(x, x) for x in args.sizes])
+    converter.convert(
+        input_image=args.input,
+        output=args.output or args.input.with_suffix(".ico"),
+        icon_sizes=[(x, x) for x in args.sizes]
+    )
 
 
 if __name__ == "__main__":
--- image2ico/cli/cli_arguments.py
+++ image2ico/cli/cli_arguments.py
@@ -5,13 +5,15 @@ from tap import Tap
 
 class CliArguments(Tap):
     input: Path
-    output: Path = Path("output.ico")
+    output: Path | None = None
     sizes: list[Literal[16, 24, 32, 48, 64, 128, 255]] = [
         16, 24, 32, 48, 64, 128, 255
     ]
 
     def configure(self) -> None:
-        self.add_argument("input")
-        self.add_argument("-o", "--output")
+        self.add_argument(
+            "input", help="input image file")
+        self.add_argument(
+            "-o", "--output", help="output ICO filename (default: input_file.ico)")
         self.add_argument(
             "-s", "--sizes", help="list of icon sizes (ex: --sizes 16 32)")
